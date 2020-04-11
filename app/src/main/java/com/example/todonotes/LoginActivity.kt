e75b5459d3e2e56592105ca2fc29c179aa300310
arti <artikumari.mca17@rvce.edu.in>
2020-04-11 16:49:06 +0530

    Fab Added

--- app/src/main/java/com/example/todonotes/LoginActivity.kt
+++ app/src/main/java/com/example/todonotes/LoginActivity.kt
@@ -1,12 +1,35 @@
 package com.example.todonotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        var login = findViewById<Button>(R.id.bt_login)
+        var fullName = findViewById<EditText>(R.id.et_fullname)
+        var username = findViewById<EditText>(R.id.et_username)
+
+        var FN = fullName.text
+        var UN = username.text
+
+        login.setOnClickListener {
+            if (!FN.isEmpty() && !UN.isEmpty()){
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+            }
+            else {
+                Log.d("Nothing", "Not logged in")
+            }
+        }
     }
 }
--- app/src/main/java/com/example/todonotes/MainActivity.kt
+++ app/src/main/java/com/example/todonotes/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.todonotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var fabNotes = findViewById<FloatingActionButton>(R.id.fab_notes)
+
+        fabNotes.setOnClickListener {
+            Log.d("Fab", "Fab clicked")
+        }
     }
 }
