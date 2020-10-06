95d21dcebf3ce6d1285fbf9ce611e8adceb48257
Arti Kumari <artianshu96@gmail.com>
2020-10-06 12:06:36 +0530

    Changes in gradle

--- app/src/main/java/com/example/todonotes/view/SplashActivity.kt
+++ app/src/main/java/com/example/todonotes/view/SplashActivity.kt
@@ -11,63 +11,87 @@ import android.widget.Toast
 import com.example.todonotes.utils.AppConstants
 import com.example.todonotes.R
 import com.example.todonotes.onBoarding.OnBoardingActivity
+import com.example.todonotes.utils.StoreSession
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.google.firebase.iid.FirebaseInstanceId
 
 
 class SplashActivity : AppCompatActivity(){
-
-    lateinit var sharedPreference : SharedPreferences
-    lateinit var editor : SharedPreferences.Editor
+    private var signInButton: SignInButton? = null
+    private var googleSignInClient : GoogleSignInClient? = null
+    private val requestCode:Int = 1
     val TAG:String = "SplashActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         setupSharedPreferences()
+        signInButton = findViewById(R.id.btn_google_sign_in)
         checkLoginStatus()
-        //getFCMToken()
     }
-
-    private fun getFCMToken() {
-        FirebaseInstanceId.getInstance().instanceId
-            .addOnCompleteListener(OnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Log.w(TAG, "getInstanceId failed", task.exception)
-                    return@OnCompleteListener
-                }
-
-                // Get new Instance ID token
-                val token = task.result?.token
-
-                // Log and toast
-
-                Log.d(TAG, token)
-                Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
-            })
-    }
-
-    private fun setupSharedPreferences() {
-            sharedPreference =
-                getSharedPreferences(AppConstants.SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE)
-        }
-
-        private fun checkLoginStatus() {
-            val isBoardingSuccess=sharedPreference.getBoolean(AppConstants.ON_BOARDED_SUCCESSFULLY,false)
-            var isLoggedin = sharedPreference.getBoolean(AppConstants.IS_LOGGED_IN, false)
-            if (isLoggedin) {
-                val intent = Intent(this@SplashActivity, MainActivity::class.java)
-                startActivity(intent)
-            }else if(isBoardingSuccess) {
-                val intent = Intent(this@SplashActivity, MainActivity::class.java)
-                startActivity(intent)
-            }
-            else {
-                val intent = Intent(this@SplashActivity, OnBoardingActivity::class.java)
-                startActivity(intent)
-            }
-
+    private fun setupSharedPreferences() { StoreSession.init(this)}
+    private fun checkLoginStatus() {
+        var isLoggedin = StoreSession.read(AppConstants.IS_LOGGED_IN)
+        if (isLoggedin!!) {
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
             finish()
+        }else{
+            googleOptions()
         }
+      //  finish()
+    }
+    private fun googleOptions() {
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+        signInButton?.setOnClickListener(View.OnClickListener {
+            signIn()
+            saveLoginStatus()
+        })
+    }
+    private fun saveLoginStatus() { StoreSession.write(AppConstants.IS_LOGGED_IN, true) }
+
+    private fun signIn() {
+        var intent = googleSignInClient?.getSignInIntent()
+        startActivityForResult(intent, requestCode)
+    }
+    override fun onActivityResult(reqCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (reqCode == requestCode){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            val account = task.result
+            saveUserData(account)
+            Toast.makeText(this, ""+StoreSession.readString(AppConstants.USER_NAME), Toast.LENGTH_SHORT).show()
+            gotoMainActivity()
+        }else{
+            Toast.makeText(this, "Error in signing In", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun gotoMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun saveUserData(account: GoogleSignInAccount?) {
+        StoreSession.write(AppConstants.USER_NAME, account?.displayName.toString())
+        StoreSession.write(AppConstants.USER_EMAIL, account?.email.toString())
+    }
 
 }
